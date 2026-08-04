23fb9cfd44d3d27074dea9513a70129d027e4b05
Goodday1989 <2012alexlst@gmail.com>
2026-08-04 10:57:13 +0800

    state structure exercise halfway

--- my-first-react-app/src/components/Practice.jsx
+++ my-first-react-app/src/components/Practice.jsx
@@ -1,10 +1,44 @@
 import { useState } from "react";
+import { initialLetters } from "../data/data.js";
+import Letter from "./Letter.jsx";
 
-export default function FeedbackForm() {
-  function handleClick() {
-    const name = prompt("What is your name?");
-    alert(`Hello, ${name}!`);
+export default function MailClient() {
+  const [letters, setLetters] = useState(initialLetters);
+  const [highlightedLetterID, setHighlightedLetterID] = useState(null);
+
+  function handleHover(letter) {
+    setHighlightedLetterID(letter?.id ?? null);
   }
 
-  return <button onClick={handleClick}>Greet</button>;
+  function handleStar(starred) {
+    setLetters(
+      letters.map((letter) => {
+        if (letter.id === starred.id) {
+          return {
+            ...letter,
+            isStarred: !letter.isStarred,
+          };
+        } else {
+          return letter;
+        }
+      }),
+    );
+  }
+
+  return (
+    <>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map((letter) => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isHighlighted={letter.id === highlightedLetterID}
+            onHover={handleHover}
+            onToggleStar={handleStar}
+          />
+        ))}
+      </ul>
+    </>
+  );
 }

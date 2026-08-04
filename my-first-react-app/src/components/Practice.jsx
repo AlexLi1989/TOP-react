9159d44482ade0e2bc31d6b3284e16388ce9caff
Goodday1989 <2012alexlst@gmail.com>
2026-08-04 14:07:57 +0800

    sharing state exercise 1 done

--- my-first-react-app/src/components/Practice.jsx
+++ my-first-react-app/src/components/Practice.jsx
@@ -1,52 +1,22 @@
 import { useState } from "react";
-import { letters } from "../data/data.js";
-import Letter from "./Letter.jsx";
 
-export default function MailClient() {
-  const [selectedId, setSelectedId] = useState([]);
-
-  const selectedCount = selectedId.length;
-
-  function handleToggle(toggledId) {
-    setSelectedId((prevSelectedId) =>
-      prevSelectedId.includes(toggledId)
-        ? prevSelectedId.filter((id) => id !== toggledId)
-        : [...prevSelectedId, toggledId],
-    );
+export default function SyncedInputs() {
+  const [text, setText] = useState("");
+  function handleChange(e) {
+    setText(e.target.value);
   }
-  // alternate and better way is to use Set instead of array,as set naturally removes duplicates
-  // const [selectedID, setSelectedId] = useState(new Set());
-  // const selectedCount = selectedId.size;
-  // function handleToggle(toggledId) {
-  //   setSelectedId((prevSelectedId) => {
-  //     const nextIds = new Set(prevSelectedIds);
-  //     if (nextIds.has(toggledId)) {
-  //       nextIds.delete(toggledId);
-  //     } else {
-  //       nextIds.add(toggledId);
-  //     }
-  //     return nextIds;
-  //   });
-  // }
-
   return (
     <>
-      <h2>Inbox</h2>
-      <ul>
-        {letters.map((letter) => (
-          <Letter
-            key={letter.id}
-            letter={letter}
-            isSelected={selectedId.includes(letter.id)}
-            //is selected={selectedID.has(letter.id)}
-            onToggle={handleToggle}
-          />
-        ))}
-        <hr />
-        <p>
-          <b>You selected {selectedCount} letters</b>
-        </p>
-      </ul>
+      <Input label="First input" value={text} onChange={handleChange} />
+      <Input label="Second input" value={text} onChange={handleChange} />
     </>
+  );
+}
+
+function Input({ label, value, onChange }) {
+  return (
+    <label>
+      {label} <input value={value} onChange={onChange} />
+    </label>
   );
 }

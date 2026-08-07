981c1e346eb8253c5fde06d99f4ab9f9fc99b37d
Goodday1989 <2012alexlst@gmail.com>
2026-08-07 11:19:11 +0800

    overuse use effect exercise 02 done

--- my-first-react-app/src/components/Practice.jsx
+++ my-first-react-app/src/components/Practice.jsx
@@ -1,17 +1,18 @@
-import { useState, useEffect } from "react";
-import { initialTodos, createTodo } from "./todos";
+import { useState, useMemo } from "react";
+import { initialTodos, createTodo, getVisibleTodos } from "./todos";
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
   const [showActive, setShowActive] = useState(false);
-  const activeTodos = todos.filter((todo) => !todo.completed);
-  const renderTodos = (todos) => {
-    return todos.map((todo) => (
-      <li key={todo.id}>{todo.completed ? <s>{todo.text}</s> : todo.text}</li>
-    ));
-  };
-  const onAdd = (text) =>
-    setTodos((prevTodos) => [...prevTodos, createTodo(text)]);
+  const [text, setText] = useState("");
+  const visibleTodos = useMemo(() => {
+    return getVisibleTodos(todos, showActive);
+  }, [todos, showActive]); // useMemo will only recompute the visibleTodos if todos or showActive change
+
+  function handleAddClick() {
+    setText("");
+    setTodos([...todos, createTodo(text)]);
+  }
 
   return (
     <>
@@ -23,25 +24,15 @@ export default function TodoList() {
         />
         Show only active todos
       </label>
-      <NewTodo onAdd={onAdd} />
-      <ul>{renderTodos(showActive ? activeTodos : todos)}</ul>
-      <footer>{activeTodos.length} todos left</footer>
-    </>
-  );
-}
-
-function NewTodo({ onAdd }) {
-  const [text, setText] = useState("");
-
-  function handleAddClick() {
-    setText("");
-    onAdd(text);
-  }
-
-  return (
-    <>
       <input value={text} onChange={(e) => setText(e.target.value)} />
       <button onClick={handleAddClick}>Add</button>
+      <ul>
+        {visibleTodos.map((todo) => (
+          <li key={todo.id}>
+            {todo.completed ? <s>{todo.text}</s> : todo.text}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
